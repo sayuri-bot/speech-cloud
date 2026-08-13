935e256940ccec97cd4d06ca920512e23da2011e
Sayuri <i2413259@continental.edu.pe>
2026-08-13 00:06:43 -0500

    Preparar aplicación para Render

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from google.cloud import speech
+from google.oauth2 import service_account
 import os
+import json
 
 app = Flask(__name__)
 
@@ -8,7 +10,17 @@ app = Flask(__name__)
 # Credenciales de Google Cloud
 # =====================================================
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credenciales.json"
+credenciales_json = os.environ.get("GOOGLE_CREDENTIALS")
+
+if credenciales_json:
+    # Render
+    info = json.loads(credenciales_json)
+    credentials = service_account.Credentials.from_service_account_info(info)
+    client = speech.SpeechClient(credentials=credentials)
+else:
+    # Desarrollo local
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credenciales.json"
+    client = speech.SpeechClient()
 
 # =====================================================
 # Página principal
@@ -32,30 +44,20 @@ def transcribir():
 
     contenido = audio.read()
 
-    client = speech.SpeechClient()
-
     audio_google = speech.RecognitionAudio(
         content=contenido
     )
 
     config = speech.RecognitionConfig(
-
         encoding=speech.RecognitionConfig.AudioEncoding.WEBM_OPUS,
-
         sample_rate_hertz=48000,
-
         language_code="es-PE",
-
         enable_automatic_punctuation=True
-
     )
 
     respuesta = client.recognize(
-
         config=config,
-
         audio=audio_google
-
     )
 
     texto = ""
@@ -64,9 +66,7 @@ def transcribir():
         texto += resultado.alternatives[0].transcript + " "
 
     return jsonify({
-
         "texto": texto.strip()
-
     })
 
 # =====================================================
@@ -78,11 +78,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
 
     app.run(
-
         host="0.0.0.0",
-
         port=port,
-
         debug=True
-
     )
